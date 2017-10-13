b75ab4f0cc9d695f3eda82471267f9215e92d4ad
ia.novozhilov <ia.novozhilov@tensor.ru>
2017-10-13 09:00:35 +0300

    Добавлено скрытие пунктов аккордеона: Наша компания, Салон

--- script/src/js/AccordionHideItems.js
+++ script/src/js/AccordionHideItems.js
@@ -1,41 +1,43 @@
 UICustomizerDefine('AccordionHideItems', ['Engine'], function (Engine) {
-   "use strict";
+  'use strict';
 
-   const selectors = {
-      'Documents': '.nav-menu-container a[data-id="documents"]',
-      'Staff': '.nav-menu-container a[data-id="staff"]',
-      'Tasks': '.nav-menu-container a[data-id="work"]',
-      'Contacts': '.nav-menu-container a[data-id="contacts"]',
-      'Calendar': '.nav-menu-container a[data-id="calendar"]',
-      'MyPage': '.nav-menu-container a[data-id="myProfile"]',
-      'Company': '.nav-menu-container a[data-id="contragents"]',
-      'Business': '.nav-menu-container a[data-id="business"]',
-      'Accounting': '.nav-menu-container a[data-id="accounting"]',
-      'UTS': '.nav-menu-container a[data-id="ca_navication"]',
-      'Telephony': '.nav-menu-container a[data-id="tel"]',
-      'Retail': '.nav-menu-container a[data-id="retail"]',
-      'Presto': '.nav-menu-container a[data-id="presto"]'
-   };
+  const selectors = {
+    'Documents': '.nav-menu-container a[data-id="documents"]',
+    'Staff': '.nav-menu-container a[data-id="staff"]',
+    'Tasks': '.nav-menu-container a[data-id="work"]',
+    'Contacts': '.nav-menu-container a[data-id="contacts"]',
+    'Calendar': '.nav-menu-container a[data-id="calendar"]',
+    'MyPage': '.nav-menu-container a[data-id="myProfile"]',
+    'OurCompany': '.nav-menu-container a[data-id="our_company"]',
+    'Company': '.nav-menu-container a[data-id="contragents"]',
+    'Business': '.nav-menu-container a[data-id="business"]',
+    'Accounting': '.nav-menu-container a[data-id="accounting"]',
+    'UTS': '.nav-menu-container a[data-id="ca_navication"]',
+    'Telephony': '.nav-menu-container a[data-id="tel"]',
+    'Retail': '.nav-menu-container a[data-id="retail"]',
+    'Presto': '.nav-menu-container a[data-id="presto"]',
+    'Booking': '.nav-menu-container a[data-id="booking"]'
+  };
 
-   return {
-      applySettings: applySettings
-   };
+  return {
+    applySettings: applySettings
+  };
 
-   function applySettings(settings) {
-      var css = '';
-      for (let groupName in settings.options) {
-         let group = settings.options[groupName];
-         for (let name in group.options) {
-            if (group.options[name].value) {
-               css += Engine.generateCSS.displayNone(selectors[name]);
-            }
-         }
+  function applySettings(settings) {
+    var css = '';
+    for (let groupName in settings.options) {
+      let group = settings.options[groupName];
+      for (let name in group.options) {
+        if (group.options[name].value) {
+          css += Engine.generateCSS.displayNone(selectors[name]);
+        }
       }
-      if (css) {
-         Engine.appendCSS('AccordionHideItems', css);
-      } else {
-         Engine.removeCSS('AccordionHideItems');
-      }
-   }
+    }
+    if (css) {
+      Engine.appendCSS('AccordionHideItems', css);
+    } else {
+      Engine.removeCSS('AccordionHideItems');
+    }
+  }
 
 });
