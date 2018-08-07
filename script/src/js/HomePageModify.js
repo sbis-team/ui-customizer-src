20c6f032529e1762193b9db817d776a44d36001a
ia.novozhilov <ia.novozhilov@tensor.ru>
2018-08-07 15:29:11 +0300

    лишнее

--- script/src/js/HomePageModify.js
+++ script/src/js/HomePageModify.js
@@ -11,11 +11,7 @@ UICustomizerDefine('HomePageModify', ['Engine'], function (Engine) {
   function applySettings(settings) {
     var css = '';
     let news = settings.options.News.options;
-    /*
-    if (news.HideAuthor.value && news.HideFooterBtn.value) {
-       css += Engine.getCSS('HomePageModify-FixHeight');
-    }
-    */
+
     for (let groupName in settings.options) {
       let group = settings.options[groupName];
       for (let name in group.options) {
@@ -25,15 +21,7 @@ UICustomizerDefine('HomePageModify', ['Engine'], function (Engine) {
         }
       }
     }
-    /*
-    if (news.HideAuthor.value && news.SlimBorder.value) {
-       css += Engine.generateCSS.custom(
-          '.sn-NewsPage .sn-DraftIcon, .sn-NewsPage .sn-FavoriteIcon, .sn-NewsPage .sn-PinIcon',
-          'top',
-          '0px !important'
-       );
-    }
-    */
+
     if (css) {
       Engine.appendCSS('HomePageModify', css);
     } else {
@@ -50,24 +38,6 @@ UICustomizerDefine('HomePageModify', ['Engine'], function (Engine) {
     } else {
       oneColumnMode();
     }
-    /*
-    Engine.waitRequire(function (require) {
-      require(['WS.Data/Source/SbisService', 'Core/UserConfig'], function (SbisService, UserConfig) {
-        let ifColumn2 = document.querySelector('.feed-All__twoColumns');
-        if (news.InOneColumn.value) {
-          if (ifColumn2) {
-            UserConfig.setParam('OnlyOneColumn', 'true');
-            toggleColumn(true);
-          }
-        } else {
-          if (!ifColumn2) {
-            UserConfig.removeParam('OnlyOneColumn');
-            toggleColumn(false);
-          }
-        }
-      });
-    });
-    */
   }
 
   function oneColumnMode() {
@@ -89,12 +59,5 @@ UICustomizerDefine('HomePageModify', ['Engine'], function (Engine) {
       setTimeout(oneColumnMode, 10);
     }
   }
-  /*
-    function toggleColumn(isOne) {
-      var news = document.querySelector('.n-NewsPageList');
-      if (news && news.wsControl) {
-        news.wsControl._setOneColumnMode(isOne, false);
-      }
-    }*/
 
 });
